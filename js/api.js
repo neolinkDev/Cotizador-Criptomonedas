1458c6fa45e8a6e1cb6b28e8e65924c350d72964
jorge <jorge@programmer.net>
2022-01-21 13:15:45 -0700

    migrando a async/await

--- js/api.js
+++ js/api.js
@@ -6,21 +6,32 @@ const getCryptocurrencies = (cryptocurrencies) =>
   new Promise((resolve) => resolve(cryptocurrencies));
 
 // función que conecta a la API
-export function checkCrypto() {
+export async function checkCrypto() {
   const url =
     "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD";
 
-  fetch(url)
-    .then((response) =>
-      response.ok ? response.json() : Promise.reject(response)
-    )
-    .then((result) => {
-      // console.log(result);
-      // console.log(result.Data);
-      return getCryptocurrencies(result.Data);
-    })
-    .then((cryptocurrencies) => cryptocurrenciesSelect(cryptocurrencies))
-    .catch((err) => console.log(err));
+  // Usando Fetch
+  // fetch(url)
+  //   .then((response) =>
+  //     response.ok ? response.json() : Promise.reject(response)
+  //   )
+  //   .then((result) => {
+  //     // console.log(result);
+  //     // console.log(result.Data);
+  //     return getCryptocurrencies(result.Data);
+  //   })
+  //   .then((cryptocurrencies) => cryptocurrenciesSelect(cryptocurrencies))
+  //   .catch((err) => console.log(err));
+
+  // Usando async/await
+  try {
+    const response = await fetch(url);
+    const result = await response.json(); 
+    const crypto = await getCryptocurrencies(result.Data);
+    cryptocurrenciesSelect(crypto);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // llenamos el select con el nombre de las criptomonedas dinamicamente con forEach
--- js/eventos.js
+++ js/eventos.js
@@ -23,7 +23,7 @@ export function formSubmit() {
 }
 
 // conectando con API para mostrar los datos de cada moneda
-function consultAPI() {
+async function consultAPI() {
   const { moneda, criptomoneda } = searchObject;
 
   const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
@@ -31,37 +31,72 @@ function consultAPI() {
   // spinner
   spinner();
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((price) => {
-      // console.log(price.DISPLAY[criptomoneda][moneda]);
+  // Usando Fetch
+  // fetch(url)
+  //   .then((response) => response.json())
+  //   .then((price) => {
+  //     // console.log(price.DISPLAY[criptomoneda][moneda]);
 
-      // limpiar HTML
-      cleanHTML();
+  //     // limpiar HTML
+  //     cleanHTML();
 
-      const { PRICE, HIGH24HOUR, CHANGEPCT24HOUR, LOW24HOUR } =
-        price.DISPLAY[criptomoneda][moneda];
+  //     const { PRICE, HIGH24HOUR, CHANGEPCT24HOUR, LOW24HOUR } =
+  //       price.DISPLAY[criptomoneda][moneda];
 
-      const $precio = document.createElement("p");
-      $precio.classList.add("precio");
-      $precio.innerHTML = `Precio: <span> ${PRICE} </span>`;
+  //     const $precio = document.createElement("p");
+  //     $precio.classList.add("precio");
+  //     $precio.innerHTML = `Precio: <span> ${PRICE} </span>`;
 
-      const $precioMasAlto24 = document.createElement("p");
-      $precioMasAlto24.innerHTML = `Precio más alto 24h: <span> ${HIGH24HOUR} </span>`;
+  //     const $precioMasAlto24 = document.createElement("p");
+  //     $precioMasAlto24.innerHTML = `Precio más alto 24h: <span> ${HIGH24HOUR} </span>`;
 
-      const $precioMasBajo24 = document.createElement("p");
-      $precioMasBajo24.innerHTML = `Precio más bajo 24h: <span> ${LOW24HOUR} </span>`;
+  //     const $precioMasBajo24 = document.createElement("p");
+  //     $precioMasBajo24.innerHTML = `Precio más bajo 24h: <span> ${LOW24HOUR} </span>`;
 
-      const $variacion24 = document.createElement("p");
-      $variacion24.innerHTML = `Variación 24h: <span> ${CHANGEPCT24HOUR}% </span>`;
+  //     const $variacion24 = document.createElement("p");
+  //     $variacion24.innerHTML = `Variación 24h: <span> ${CHANGEPCT24HOUR}% </span>`;
 
-      resultado.appendChild($precio);
-      resultado.appendChild($precioMasAlto24);
-      resultado.appendChild($precioMasBajo24);
-      resultado.appendChild($variacion24);
+  //     resultado.appendChild($precio);
+  //     resultado.appendChild($precioMasAlto24);
+  //     resultado.appendChild($precioMasBajo24);
+  //     resultado.appendChild($variacion24);
 
-      formulario.appendChild(resultado);
-    });
+  //     formulario.appendChild(resultado);
+  //   });
+
+  // Usando Async/Await
+  try {
+    const response = await fetch(url);
+    const price = await response.json();
+  
+    // limpiar HTML
+    cleanHTML();
+  
+    const { PRICE, HIGH24HOUR, CHANGEPCT24HOUR, LOW24HOUR } =
+      price.DISPLAY[criptomoneda][moneda];
+  
+    const $precio = document.createElement("p");
+    $precio.classList.add("precio");
+    $precio.innerHTML = `Precio: <span> ${PRICE} </span>`;
+  
+    const $precioMasAlto24 = document.createElement("p");
+    $precioMasAlto24.innerHTML = `Precio más alto 24h: <span> ${HIGH24HOUR} </span>`;
+  
+    const $precioMasBajo24 = document.createElement("p");
+    $precioMasBajo24.innerHTML = `Precio más bajo 24h: <span> ${LOW24HOUR} </span>`;
+  
+    const $variacion24 = document.createElement("p");
+    $variacion24.innerHTML = `Variación 24h: <span> ${CHANGEPCT24HOUR}% </span>`;
+  
+    resultado.appendChild($precio);
+    resultado.appendChild($precioMasAlto24);
+    resultado.appendChild($precioMasBajo24);
+    resultado.appendChild($variacion24);
+  
+    formulario.appendChild(resultado);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // llena el objeto con el tipo de moneda y criptomoneda seleccionada
